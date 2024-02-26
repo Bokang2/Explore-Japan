d3db07ccfeb77eb2d93c6c6bf3ada757ebde6c6e
Bokang <147839054+Bokang2@users.noreply.github.com>
2024-02-26 13:20:54 +0000

    Add files via upload

---
+++ ExploreJapan/js/common.js
@@ -0,0 +1,10 @@
+// Add active class to the current button (highlight it)
+// var header = document.getElementById("navbar");
+// var pages = header.getElementsByClassName("page");
+// for (var i = 0; i < btns.length; i++) {
+//     pages[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("active");
+//   current[0].className = current[0].className.replace(" active", "");
+//   this.className += " active";
+//   });
+// }
